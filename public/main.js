034f31775858f37e6c92d408e873800c39f0d132
fromdenisvieira <fromdenisvieira@live.com>
2015-05-03 16:32:17 -0300

    first functionality

---
+++ public/main.js
@@ -0,0 +1,21 @@
+var app = angular.module('chatApp',[]);
+
+app.factory('socket',function(){
+	var socket = io.connect('http://localhost:3000')
+	return socket;
+})
+
+app.controller('ChatCtrl',function($scope,socket){
+	$scope.msgs = [];
+	$scope.sendMsg = function(){
+		socket.emit('send msg',$scope.msg.text);
+		$scope.msg.text = '';
+	}
+
+	socket.on('get msg',function(data){
+		$scope.msgs.push(data);
+		$scope.$digest();
+
+	})
+
+})
